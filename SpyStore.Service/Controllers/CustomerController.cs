19dced90eb9f03a187ea1653946c436b09dc85a4
arnoldwilliams <arnoldwilliams@hotmail.com>
2020-09-06 23:04:13 +0100

    Initial Commit

---
+++ SpyStore.Service/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SpyStore.Dal.Repos;
+using SpyStore.Dal.Repos.Interfaces;
+using SpyStore.Models.Entities;
+
+namespace SpyStore.Service.Controllers
+{
+    [Route("api/customer")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerRepo _repo;
+
+        public CustomerController(ICustomerRepo repo)
+        {
+            _repo = repo;
+        }
+    }
+}
---
+++ SpyStore.Service/Startup.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using SpyStore.Dal.EfStructures;
+using SpyStore.Dal.Initialization;
+using SpyStore.Dal.Repos;
+using SpyStore.Dal.Repos.Interfaces;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.IO;
+using System.Reflection;
+using SpyStore.Service.Filters;
+
+namespace SpyStore.Service
+{
+    public class Startup
+    {
+        public readonly IHostingEnvironment _env;
+
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            Configuration = configuration;
+            _env = env;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddMvcCore(
+            config => config.Filters.Add(new SpyStoreExceptionFilter(_env)))
+                .AddJsonFormatters(j =>
+                {
+                    j.ContractResolver = new DefaultContractResolver();
+                    j.Formatting = Formatting.Indented;
+                });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", builder =>
+                {
+                    builder
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowAnyOrigin()
+                        .AllowCredentials();
+                });
+            });
+
+            services.AddDbContextPool<StoreContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("SpyStore")));
+
+            services.AddScoped<ICategoryRepo, CategoryRepo>();
+            services.AddScoped<IProductRepo, ProductRepo>();
+            services.AddScoped<ICustomerRepo, CustomerRepo>();
+            services.AddScoped<IShoppingCartRepo, ShoppingCartRepo>();
+            services.AddScoped<IOrderRepo, OrderRepo>();
+            services.AddScoped<IOrderDetailRepo, OrderDetailRepo>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new Info
+                    {
+                        Title = "SpyStore Service",
+                        Version = "v1",
+                        Description = "Service to support the SpyStore sample eCommerce site",
+                        TermsOfService = "None",
+                        License = new License
+                        {
+                            Name = "Freeware",
+                            //Url = "https://en.wikipedia.org/wiki/Freeware"
+                            Url = "http://localhost:23741/LICENSE.txt"
+                        }
+                    });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                using (var serviceScope =
+                    app
+                        .ApplicationServices
+                        .GetRequiredService<IServiceScopeFactory>()
+                        .CreateScope())
+                {
+                    var context =
+                        serviceScope.ServiceProvider.GetRequiredService<StoreContext>();
+                    SampleDataInitializer.InitializeData(context);
+                }
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SpyStore Service v1");
+            });
+            app.UseStaticFiles();
+
+            app.UseCors("AllowAll");
+
+            app.UseMvc();
+        }
+    }
+}
